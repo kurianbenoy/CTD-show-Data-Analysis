2305f74313496d53a1cffd30b0caac75f292bec3
kurianbenoy <kurian.bkk@gmail.com>
2020-07-12 20:52:50 +0530

    add statistical nlp code

---
+++ NLP-bootcamp/text_representation.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+import nltk
+import re
+
+
+nltk.download('stopwords')
+nltk.download('punkt')
+
+
+corpus = ['The sky is blue and beautiful.',
+          'Love this blue and beautiful sky!',
+          'The quick brown fox jumps over the lazy dog.',
+          "A king's breakfast has sausages, ham, bacon, eggs, toast and beans",
+          'I love green eggs, ham, sausages and bacon!',
+          'The brown fox is quick and the blue dog is lazy!',
+          'The sky is very blue and the sky is very beautiful today',
+          'The dog is lazy but the brown fox is quick!'
+]
+labels = ['weather', 'weather', 'animals', 'food', 'food', 'animals', 'weather', 'animals']
+
+corpus = np.array(corpus)
+corpus_df = pd.DataFrame({'Document': corpus,
+                          'Category': labels})
+corpus_df = corpus_df[['Document', 'Category']]
+
+stop_words = nltk.corpus.stopwords.words('english')
+
+def normalize_document(doc):
+    # lower case and remove special characters\whitespaces
+    doc = re.sub(r'[^a-zA-Z\s]', '', doc, re.I|re.A)
+    doc = doc.lower()
+    doc = doc.strip()
+    # tokenize document
+    tokens = nltk.word_tokenize(doc)
+    # filter stopwords out of document
+    filtered_tokens = [token for token in tokens if token not in stop_words]
+    # re-create document from filtered tokens
+    doc = ' '.join(filtered_tokens)
+    return doc
+
+normalize_corpus = np.vectorize(normalize_document)
+
+norm_corpus = normalize_corpus(corpus)
+print(norm_corpus)
+
